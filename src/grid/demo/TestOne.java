3c6cf192689278dd16f361278322d340d65639ed
balaram <balaram1985behera@gmail.com>
2018-06-13 23:32:00 +0530

    First commit for NamSelFour

---
+++ src/grid/demo/TestOne.java
@@ -0,0 +1,19 @@
+package grid.demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TestOne {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
